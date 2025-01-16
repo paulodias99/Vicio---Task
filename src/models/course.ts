f8beb29b9c2835b395ee3734a3bdac863c8110cc
Paulo Dias <paulodiasprivado@gmail.com>
2025-01-16 15:18:59 -0300

    refactor(models): unify models structure

--- src/models/course.ts
+++ src/models/course.ts
@@ -1,9 +1,35 @@
-import { Prisma } from '@prisma/client';
+import prisma from "../config/prisma";
+import { Prisma } from "@prisma/client";
 
-export interface Course extends Prisma.CourseCreateInput {
-    id: number;
-    title: string;
-    description: string;
-    hours: number;
-    createdAt: Date;
-}
+export type CourseData = Prisma.CourseCreateInput;
+
+const CourseModel = {
+  async create(data: CourseData) {
+    return prisma.course.create({ data });
+  },
+
+  async findById(id: number) {
+    return prisma.course.findUnique({
+      where: { id },
+    });
+  },
+
+  async findAll() {
+    return prisma.course.findMany();
+  },
+
+  async update(id: number, data: Partial<CourseData>) {
+    return prisma.course.update({
+      where: { id },
+      data,
+    });
+  },
+
+  async delete(id: number) {
+    return prisma.course.delete({
+      where: { id },
+    });
+  },
+};
+
+export default CourseModel;
